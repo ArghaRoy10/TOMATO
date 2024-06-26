98fadcccd0bdad209dcdd8fcb516cbd08a74eb81
Argha Roy <135490193+ArghaRoy10@users.noreply.github.com>
2024-06-26 22:25:51 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  your mongodb url
+  await mongoose.connect('mongodb+srv://royargha2002:riju10@cluster0.pxjtezp.mongodb.net/food-deli').then(()=>console.log("DB Connected"));
 }
